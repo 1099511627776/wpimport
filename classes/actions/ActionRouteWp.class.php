e741214598fa7288d6839aa3aa4a105511cfbb0a
gigabyte <1099511627776@mail.ru>
2013-05-10 10:54:27 +0300

    issue #6 closed

--- classes/actions/ActionRouteWp.class.php
+++ classes/actions/ActionRouteWp.class.php
@@ -15,33 +15,33 @@
 
 class PluginWpimport_ActionRouteWp extends ActionPlugin_Inherit_ActionError {
 
-	protected function EventError() {
-		$url = $this->getParam(0);
-		$fullurl = $_SERVER['REQUEST_URI'];
-		if(preg_match('/\/(\S+)\.html.*?/i',$fullurl,$matches)){
-			$alias = $matches[1];
-			if($iid = $this->PluginWpimport_wpimport_getTopicIdByAlias($alias)){
-				if(!($oTopic = $this->PluginWpimport_lstopic_getTopicByK2Id($iid))){					
-					$this->PluginWpimport_wpimport_addPost($iid);
-					$this->PluginWpimport_wpimport_addComments($iid);
-					if($oTopic = $this->PluginWpimport_lstopic_getTopicByK2Id($iid)){
-						header('HTTP/1.1 301 Moved Permanently');
-						header('Location: '.$oTopic->getUrl());				
-						dump("I'm routing to:".$oTopic->getUrl());
-						return;
-					} else {
-						parent::EventError();
-					}
-				} else {
-					header('HTTP/1.1 301 Moved Permanently');
-					header('Location: '.$oTopic->getUrl());
-					dump("I'm routing to:".$oTopic->getUrl());
-					return;
-				}		
-			}
-		} 
-		parent::EventError();
-	}
+    protected function EventError() {
+        $url = $this->getParam(0);
+        $fullurl = $_SERVER['REQUEST_URI'];
+        if(preg_match('/\/(\S+)\.html.*?/i',$fullurl,$matches)){
+            $alias = $matches[1];
+            if($iid = $this->PluginWpimport_wpimport_getTopicIdByAlias($alias)){
+                if(!($oTopic = $this->PluginWpimport_lstopic_getTopicByWPId($iid))){
+                    $this->PluginWpimport_wpimport_addPost($iid);
+                    $this->PluginWpimport_wpimport_addComments($iid);
+                    if($oTopic = $this->PluginWpimport_lstopic_getTopicByWPId($iid)){
+                        header('HTTP/1.1 301 Moved Permanently');
+                        header('Location: '.$oTopic->getUrl());
+                        dump("I'm routing to:".$oTopic->getUrl());
+                        return;
+                    } else {
+                        parent::EventError();
+                    }
+                } else {
+                    header('HTTP/1.1 301 Moved Permanently');
+                    header('Location: '.$oTopic->getUrl());
+                    dump("I'm routing to:".$oTopic->getUrl());
+                    return;
+                }       
+            }
+        } 
+        parent::EventError();
+    }
 }
 
 ?>
